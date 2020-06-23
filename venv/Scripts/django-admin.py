17b4ad368df7833615e2569a455d094b392b44bd
Aayushi Gupta <aayushigupta0209@gmail.com>
2020-06-23 17:22:35 +0530

    first commit

---
+++ venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Aayushi Gupta\Desktop\prgs\phy\djangoproject3\venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
